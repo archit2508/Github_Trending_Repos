ef045fcb1976a062f36bc8433c9d7d9c66067d14
archit69 <architgupta690@gmail.com>
2020-02-02 13:17:18 +0530

    Base application class injected to AppComponent

---
+++ app/src/main/java/com/example/top_github/BaseApplication.kt
@@ -0,0 +1,13 @@
+package com.example.top_github
+
+import com.example.top_github.di.DaggerApplicationComponent
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
+
+class BaseApplication : DaggerApplication() {
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerApplicationComponent.builder().application(this).build()
+    }
+
+}
---
+++ app/src/main/java/com/example/top_github/di/components/ApplicationComponent.kt
@@ -0,0 +1,22 @@
+package com.example.top_github.di.components
+
+import android.app.Application
+import com.example.top_github.BaseApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+
+@Component(modules = [AndroidSupportInjectionModule::class])
+interface ApplicationComponent : AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): ApplicationComponent
+    }
+
+}
